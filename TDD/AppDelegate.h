1e77ba96be6d9d6df0fa90049659f05670a757df
杨绍鑫 <yangshaoxin@yangshaoxindemac-mini.local>
2015-11-09 11:24:28 +0800

    Initial Commit

---
+++ TDD/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TDD
+//
+//  Created by 杨绍鑫 on 15/11/9.
+//  Copyright © 2015年 test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TDD/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TDD
+//
+//  Created by 杨绍鑫 on 15/11/9.
+//  Copyright © 2015年 test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
